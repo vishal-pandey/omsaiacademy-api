d2768d8bc929910f459facb1809b35fbd2023717
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-13 00:31:33 +0530

    script upload

--- db/build.sql
+++ db/build.sql
@@ -1,4 +1,4 @@
 
 
 
-CREATE TABLE faculty (id int primary key, name varchar(150), quali text, mobile varchar(20), about text , img text );
+CREATE TABLE faculty (id int primary key auto_increment, name varchar(150), quali text, mobile varchar(20), about text , img text );
---
+++ faculty/addteacher.php
@@ -0,0 +1,26 @@
+<?php
+	include "../db/connect.php";
+	// $id = $_POST['id'];
+	$name = $_POST['name'];
+	$quali = $_POST['quali'];
+	$mobile = $_POST['mobile'];
+	$about = $_POST['about'];
+
+	$sql = "insert into faculty (name, quali, mobile, about) VALUES ('{$name}'.'{$quali}'.'{$mobile}'.'{$about}')";
+	$result = $conn->query($sql);
+
+	if ($result) {
+		echo "success";
+	}
+
+	// $obj = new StdClass();
+
+	
+	// $obj = $result->fetch_object();
+
+	// if (json_encode($obj) != "") {
+	// 	echo json_encode($obj);
+	// }
+	
+
+?>
