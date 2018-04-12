cb4cd4dc0153f32ec6078a6848a03817c135a6a2
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-13 01:26:19 +0530

    script upload

---
+++ faculty/deleteteacher.php
@@ -0,0 +1,14 @@
+<?php
+	include "../db/connect.php";
+	// $id = $_POST['id'];
+	
+	$sql = "delete from faculty where id = '{$id}'";
+	$result = $conn->query($sql);
+
+	if ($result) {
+		echo "success";
+	}else{
+		echo $sql;
+	}	
+
+?>
