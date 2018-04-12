1dccba108c1077659cdf5220e5f1524c709a967a
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-12 22:33:03 +0530

    script upload

---
+++ faculty/getteacher.php
@@ -0,0 +1,18 @@
+<?php
+	include "../db/connect.php";
+	$id = $_POST['id'];
+
+	$sql = "select * from faculty where id = ".$id;
+
+	$result = $conn->query($sql);
+	$obj = new StdClass();
+
+	
+	$obj = $result->fetch_object() 
+
+	if (json_encode($obj) != "") {
+		echo json_encode($obj);
+	}
+	
+
+?>
