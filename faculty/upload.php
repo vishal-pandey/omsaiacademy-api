65946a4c0b42271a20f142c5e8e3126a9c9f207a
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-13 02:08:08 +0530

    script upload

--- faculty/upload.php
+++ faculty/upload.php
@@ -3,8 +3,6 @@
 	$photo = $_FILES['photo']['name'];
 	$id = $_POST['id'];
 	$target_file = $dir.$id.".jpg";
-	echo $photo;
-	echo $id;
 	if(move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)){
 		echo "Uploaded";
 	}
