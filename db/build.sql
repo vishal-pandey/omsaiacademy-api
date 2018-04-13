7e759004ea760899832588d7b523b19231f74216
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-13 23:38:16 +0530

    script upload

--- db/build.sql
+++ db/build.sql
@@ -1,10 +1,4 @@
-
-
-
 CREATE TABLE faculty (id int primary key auto_increment, name varchar(150), quali text, mobile varchar(20), about text , img text );
-
-
-
 CREATE TABLE batch (id int primary key auto_increment, batch text, btime text, day text);
-
 insert into batch (batch, btime, day) values ("11th", "11 AM", "Sunday");
+CREATE TABLE youtube (id int primary key auto_increment, link text);
---
+++ gallery/uploadslider.php
@@ -0,0 +1,8 @@
+<?php
+	$dir = "./slider/";
+	$photo = $_FILES['photo']['name'];
+	$target_file = $dir.$photo.".jpg";
+	if(move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)){
+		echo "Uploaded";
+	}
+?>
