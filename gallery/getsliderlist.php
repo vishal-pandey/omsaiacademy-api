056507e8e77ab16025a17200e4f0fd01fe8bd675
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-13 23:55:40 +0530

    script upload

--- gallery/getsliderlist.php
+++ gallery/getsliderlist.php
@@ -1,7 +1,7 @@
 <?php
 	$dir = "./slider";
-	// $img = scandir($dir);
-	$img = array_diff(scandir($dir), array('..', '.'));
+	$img = scandir($dir);
+	// $img = array_diff(scandir($dir), array('..', '.'));
 	echo json_encode($img);
 	// print_r($img);
 ?>
