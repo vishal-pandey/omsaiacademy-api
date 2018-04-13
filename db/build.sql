a9d685a079555058d08f6e3ea51714da2ca7dd29
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-13 15:07:31 +0530

    script upload

--- db/build.sql
+++ db/build.sql
@@ -2,3 +2,7 @@
 
 
 CREATE TABLE faculty (id int primary key auto_increment, name varchar(150), quali text, mobile varchar(20), about text , img text );
+
+
+
+CREATE TABLE batch (id int primary key auto_increment, batch text, btime text, day text);
