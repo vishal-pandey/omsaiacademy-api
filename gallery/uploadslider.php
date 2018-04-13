5a6970b601349f0c1975a8c60aa0b826152aa536
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-13 23:42:15 +0530

    script upload

--- gallery/uploadslider.php
+++ gallery/uploadslider.php
@@ -1,7 +1,7 @@
 <?php
 	$dir = "./slider/";
 	$photo = $_FILES['photo']['name'];
-	$target_file = $dir.$photo.".jpg";
+	$target_file = $dir.$photo;
 	if(move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)){
 		echo "Uploaded";
 	}
